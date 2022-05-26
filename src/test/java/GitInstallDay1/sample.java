c53ab23c5485b03c6df5917e20db4e87e60c9df3
sathishravi200 <sathishravi200@gmail.com>
2022-05-27 01:19:30 +0530

    GitInstall

---
+++ src/test/java/GitInstallDay1/sample.java
@@ -0,0 +1,17 @@
+package GitInstallDay1;
+
+public class sample {
+	
+	public void method1() {
+
+		System.out.println("Method1");
+	}
+	public void method2() {
+
+		System.out.println("Method2");
+}
+	public void method3() {
+		System.out.println("Method3");
+	}
+
+}
