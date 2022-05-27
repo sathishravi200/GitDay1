ee9a5329ae3e0a7e52b318585d20488332c0f7f2
Vinoth1923 <vinothvenkittu11@gmail.com>
2022-05-28 00:07:10 +0530

    added methods

--- src/test/java/GitInstallDay1/sample.java
+++ src/test/java/GitInstallDay1/sample.java
@@ -13,5 +13,14 @@ public class sample {
 	public void method3() {
 		System.out.println("Method3");
 	}
+	private void maths() {
+		System.out.println("maths");
+	}
+	private void science() {
+		System.out.println("science");
+	}
+	private void social() {
+		System.out.println("social");
+	}
 
 }
